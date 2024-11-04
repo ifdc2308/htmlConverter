10397a701b28cda9929a956a609b1e4489946629
@ifdc2308 <italodorea123@gmail.com>
2024-11-04 11:53:31 -0300

    feat: html-converter

--- script.js
+++ script.js
@@ -9,15 +9,3 @@ document.getElementById("submitBtn").addEventListener("click", function () {
 
   localStorage.setItem("valor inserido", textInsert);
 });
-
-// text.html
-document.getElementById("submitText").addEventListener("click", function () {
-  var editor = tinymce.get("textEditor");
-  var content = editor.getContent({ format: "raw" });
-
-  // Exibe o conteúdo HTML puro no resultadoText
-  document.getElementById("resultadoText").textContent = content;
-
-  // Log no console do valor capturado
-  console.log("Valor HTML puro do editor:", content);
-});
